0cd97c9e8d821e303882aad4b53ee00a97edb681
Ben Stein <bumstain@gmail.com>
2016-05-26 22:40:19 +1000

    Added validations for reviews

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Restaurant, type: :model do
+
+  it { is_expected.to have_many :reviews }
+
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
@@ -11,5 +14,25 @@ describe Restaurant, type: :model do
     Restaurant.create(name: "Moe's Tavern")
     restaurant = Restaurant.new(name: "Moe's Tavern")
     expect(restaurant).to have(1).error_on(:name)
+  end
+
+end
+
+describe 'reviews' do
+  describe 'build_with_user' do
+
+    let(:user) { User.create email: 'test@test.com' }
+    let(:restaurant) { Restaurant.create name: 'Test' }
+    let(:review_params) { {rating: 5, thoughts: 'yum'} }
+
+    subject(:review) { restaurant.reviews.build_with_user(review_params, user) }
+
+    it 'builds a review' do
+      expect(review).to be_a Review
+    end
+
+    it 'builds a review associated with the specified user' do
+      expect(review.user).to eq user
+    end
   end
 end
